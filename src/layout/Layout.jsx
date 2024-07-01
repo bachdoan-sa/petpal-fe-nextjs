decc05ee9d27bcb6653851ae7ab0e1b76eed0b3f
VoPhucTin <86974800+VoPhucTin@users.noreply.github.com>
2024-07-01 18:51:40 +0700

    fix_ui

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Footer1 from "../components/footer/Footer1";
 import Header1 from "../components/header/Header1";
+import Header2 from "../components/header/Header2";
 
 function Layout({ children }) {
   return (
     <>
-      <Header1 />
+      <Header2 />
       {children}
       <Footer1 />
     </>
