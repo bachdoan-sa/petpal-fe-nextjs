9e33c6daff95d2d9722cdf851542c8cc20711c7e
bachdoan-sa <bach.doansy@gmail.com>
2024-07-11 22:51:20 +0700

    update navbar notification

--- src/admin-components/layouts/QuickMenu.jsx
+++ src/admin-components/layouts/QuickMenu.jsx
@@ -4,11 +4,11 @@ import { Fragment } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 // // simple bar scrolling used for notification item scrolling
-// import SimpleBar from 'simplebar-react';
-// import 'simplebar/dist/simplebar.min.css';
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 
-// // import data files
-// import NotificationList from 'data/Notification';
+// import data files
+import NotificationList from '../../data/Notification';
 
 // import hooks
 import useMounted from '../../hooks/useMounted';
@@ -21,66 +21,70 @@ function QuickMenu() {
         query: '(min-width: 1224px)'
     })
 
-    // function Notifications () {
-    //     return (
-    //         <SimpleBar style={{ maxHeight: '300px' }}>
-    //             <ListGroup variant="flush">
-    //                 {NotificationList.map(function (item, index) {
-    //                     return (
-    //                         <ListGroup.Item className={index === 0 ? 'bg-light' : ''} key={index}>
-    //                             <Row>
-    //                                 <Col>
-    //                                     <Link href="#" className="text-muted">
-    //                                         <h5 className=" mb-1">{item.sender}</h5>
-    //                                         <p className="mb-0"> {item.message}</p>
-    //                                     </Link>
-    //                                 </Col>
-    //                             </Row>
-    //                         </ListGroup.Item>
-    //                     );
-    //                 })}
-    //             </ListGroup>
-    //         </SimpleBar>
-    //     );
-    // }
+    const Notifications = () => {
+        return (
+            <SimpleBar style={{ maxHeight: '300px', overflowX: 'hidden' }} autoHide={true}>
+                <ul class="list-group list-group-flush">
+                    {NotificationList.map(function (item, index) {
+                        return (
+                            <li className={"list-group-item " + (index === 0 ? 'bg-light' : '')} key={index}>
+                                <Link href="#" className="text-muted">
+                                    <h5 className=" mb-1">{item.sender}</h5>
+                                    <p className="mb-0"
+                                        style={{
+                                            overflow: "hidden",
+                                            textOverflow: "ellipsis",
+                                            whiteSpace: "nowrap"
+                                        }}>
+                                        {item.message}
+                                    </p>
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </SimpleBar >
+        );
+    }
 
     function QuickMenuDesktop() {
         return (
-            <ul className=" navbar-nav navbar-right-wrap ms-auto d-flex nav-top-wrap" >
-                <li className=" stopevent" role="presentation">
-                    <div className="dropdown">
+            <div className=" navbar-nav navbar-right-wrap ms-auto d-flex nav-top-wrap" >
+                <div className=" stopevent" role="presentation">
+                    <span className="dropdown">
                         <button id="dropdownNotification" className="btn btn-light btn-icon rounded-circle indicator indicator-primary text-muted" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <i className="fe fe-bell"></i>
                         </button>
-                        <ul className="dashboard-dropdown notifications-dropdown dropdown-menu dropdown-menu-lg dropdown-menu-end py-0 show" aria-labelledby="dropdownNotification">
-                            <li className="dropdown-item mt-3" role="presentation">
-                                <div className="border-bottom px-3 pt-0 pb-3 d-flex justify-content-between align-items-end">
+                        <div className="dashboard-dropdown notifications-dropdown dropdown-menu dropdown-menu-lg dropdown-menu-end py-0 show" aria-labelledby="dropdownNotification">
+                            <div className="dropdown-item mt-3" role="presentation">
+                                <span className="border-bottom px-3 pt-0 pb-3 d-flex justify-content-between align-items-end">
                                     <span className="h4 mb-0">Notifications</span>
                                     <Link href="/" className="text-muted">
                                         <span className="align-middle">
                                             <i className="fe fe-settings me-1"></i>
                                         </span>
                                     </Link>
-                                </div>
-                                <div className="border-top px-3 pt-3 pb-3">
+                                </span>
+                                <Notifications />
+                                <span className="border-top px-3 pt-3 pb-3">
                                     <a href="/dashboard/notification-history" className="text-link fw-semi-bold">See all Notifications</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li className="ms-2" role="presentation">
-                    <div className="dropdown" >
-                        <button id="dropdownUser" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{borderRadius:"50%"}}>
-                            <img alt="avatar" src='/images/avatar/avatar-1.jpg' className="rounded-circle avatar avatar-md avatar-indicators avatar-online" />
+                                </span>
+                            </div>
+                        </div>
+                    </span>
+                </div>
+                <div className="ms-2" role="presentation">
+                    <span className="dropdown" >
+                        <button id="dropdownUser" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ borderRadius: "50%" }}>
+                            <img alt="avatar" src='assets/images/blog/blog-author.png' className="rounded-circle avatar avatar-md avatar-indicators avatar-online" />
                         </button>
                         <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownUser" show>
                             <li className="dropdown-item px-4 pb-0 pt-2" role="presentation">
-                                <div className="lh-1">
+                                <span className="lh-1">
                                     <h5 className="mb-1"> John E. Grainger</h5>
                                     <Link href="#" className="text-inherit fs-6">View my profile</Link>
-                                </div>
-                                <div className="dropdown-divider mt-3 mb-2"></div>
+                                </span>
+                                <span className="dropdown-spanider mt-3 mb-2"></span>
                             </li>
                             <li className="dropdown-item"><i className="fe fe-user me-2"></i> Edit Profile</li>
                             <li className="dropdown-item"><i className="fe fe-activity me-2"></i> Activity Log</li>
@@ -88,16 +92,16 @@ function QuickMenu() {
                             <li className="dropdown-item"><i className="fe fe-settings me-2"></i> Account Settings</li>
                             <li className="dropdown-item"><i className="fe fe-power me-2"></i>Sign Out</li>
                         </ul>
-                    </div>
-                </li>
-            </ul>
+                    </span>
+                </div>
+            </div>
         )
     }
 
     function QuickMenuMobile() {
         return (
             <>
-                {/* unsupport */}
+                {/* not yet support */}
             </>
         )
     }
--- src/hooks/useMounted.js
+++ src/hooks/useMounted.js
@@ -1,4 +1,5 @@
-// Cái này dùng để fix React Hydration Error trong trang admin (ex: <div> in <li>)
+// Cái này dùng để fix React Hydration Error trong trang admin (ex: <div> in <li>) 
+// Có thể không dùng cái này nếu đổi thẻ <div> thành <span> nhưng mà hiển thị sẽ không đẹp với phần noti
 
 import { useState, useEffect } from 'react';
 
