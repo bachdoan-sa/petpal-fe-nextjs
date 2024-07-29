04008861a8bed4eaf50f0fef778518905256f94c
bachdoan-sa <bach.doansy@gmail.com>
2024-07-29 19:18:37 +0700

    add manager get order

--- src/apiRequests/order.ts
+++ src/apiRequests/order.ts
@@ -1,11 +1,20 @@
 import http from '@/src/lib/httpAxios'
 import {
-  CreateOrderBodyType,
-  CreateOrderResType
+    CreateOrderBodyType,
+    CreateOrderResType,
+    OrderListPageBodyType,
+    OrderListPageResType
 } from '@/src/schemaValidations/order.schema'
 import { MessageResType } from '@/src/schemaValidations/common.schema'
 
 const orderApiRequest = {
-    userCreateOrder: (body: CreateOrderBodyType) => http.post<CreateOrderResType>('/api/Order/create-order-request', body)
-  }
-  export default orderApiRequest;
+    userCreateOrder: (body: CreateOrderBodyType) => http.post<CreateOrderResType>('/api/Order/create-order-request', body),
+    getListOrderForManager: ({ body, sessionToken }: { body: OrderListPageBodyType; sessionToken?: string }) =>
+        http.post<OrderListPageResType>("/api/Order/get-order-request", body,
+            {
+                headers: {
+                    Authorization: `Bearer ${sessionToken}`
+                }
+            })
+}
+export default orderApiRequest;
--- src/schemaValidations/order.schema.ts
+++ src/schemaValidations/order.schema.ts
@@ -5,13 +5,13 @@ import { PagingRes, PagingBody } from "./paging/paging.schema";
 // Các bước định nghĩa 1 response
 // 1. định nghĩa object đó
 export const OrderSchema = z.object({
-    id: z.string(),
+    id: z.string().optional(),
     currentPrice: z.number(),
     fromDate: z.string(),
     toDate: z.string(),
     receiveTime: z.number(),
     returnTime: z.string(),
-    status: z.string(),
+    status: z.string().optional(),
     userId: z.string(),
     petId: z.string(),
     packageId: z.string()
@@ -36,6 +36,7 @@ export const OrderListRes = z.object({
     message: z.string()
 })
 // 3.2
+export type  OrderListType = z.TypeOf<typeof OrderList>
 export type OrderListResType = z.TypeOf<typeof OrderListRes>
 
 // 4. Định nghĩa cấu trúc trả về theo list (có PAGINATION)
